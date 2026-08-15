0bec36e7b40a6ab4c16ec9f6049eff2ed35f75f5
João Campos <joaomarcelo_1000@hotmail.com>
2026-08-15 11:25:02 -0300

    test: Adiciona classe inicial de teste.

---
+++ src/test/java/dev/java10x/FinanceControl/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.FinanceControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
